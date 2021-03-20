b2f87703285be2fd60f26d0d9fb77d14f7c2a566
Dayana Panova <dzopino@abv.bg>
2021-03-20 12:44:40 +0200

    making an ingridient component and styling the different ingridients. Also adding the dynamically

--- src/components/Burger/BurgerIngridietn/BurgerIngridient.js
+++ src/components/Burger/BurgerIngridietn/BurgerIngridient.js
@@ -1,7 +1,33 @@
 import React from 'react';
+import classes from './burgerIngrident.css'
 
 const burgerIngrident = (props) => {
+    let ingridient = null;
 
+    switch(props.type) {
+        case('bread-bottom'):
+            ingridient = <div className={classes.BreadBottom}></div>
+        break;
+        case('bread-top'): 
+            ingridient = <div className={classes.BreadTop}>
+                        <div className={classes.Seeds1}></div>
+                        <div className={classes.Seeds2}></div>
+                    </div>
+        break;
+        case('meat'): 
+            ingridient = <div className={classes.Meat}></div>
+        break;
+        case('cheese'):
+            ingridient = <div className={classes.Cheese}></div>
+        break;
+        case('salad'): 
+            ingridient = <div className={classes.Salad}></div>
+        break;
+        case('bacon'): 
+            ingridient = <div className={classes.Bacon}></div>
+        default:
+            ingridient = null;
+    }
 }
 
 export default burgerIngrident;
