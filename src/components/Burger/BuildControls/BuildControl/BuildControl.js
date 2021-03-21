423e7908ee6888f76663cc1975adea4744eb2163
Dayana Panova <dzopino@abv.bg>
2021-03-21 23:41:53 +0200

    adding a build controls and a build control folder, and adding a build control css file

---
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const buildControl = (props) => {
+    <div>
+        <div>{props.label}</div>
+        <div>Less</div>
+        <div>More</div>
+    </div>
+}
+
+export default buildControl;
