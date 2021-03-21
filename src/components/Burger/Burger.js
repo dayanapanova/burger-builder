33f75b117b0655cf58c48b2b2c748e1a8ceef49c
Dayana Panova <dzopino@abv.bg>
2021-03-21 22:47:50 +0200

    adding state to burger ingridient component

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,16 @@ import BurgerIngridient from './BurgerIngridient/BurgerIngridient'
 
 
 const burger = (props) => {
+    const transformedIngridients = Object.keys(props.ingridients)
+    .map(igKey => {
+        return [...Array(props.ingridients[igKey])].map((_, i) => {
+            return <BurgerIngridient key={igKey + i} type={igKey}/>
+        })
+    })
     return(
         <div className={classes.Burger}>
             <BurgerIngridient type="bread-top"/>
-            <BurgerIngridient type="meat"/>
-            <BurgerIngridient type="cheese"/>
+            {transformedIngridients}
             <BurgerIngridient type="bread-top"/>
         </div>
     )
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,6 +3,14 @@ import Auxilliary from '../../hoc/Auxilliary'
 import Burger from '../../components/Burger/Burger'
 
 class BurgerBuilder extends Component {
+    state = {
+        ingridients: {
+            salad: 0,
+            bacon:0,
+            cheese: 0,
+            meat:0
+        }
+    }
     render() {
         return(
             <Auxilliary>
