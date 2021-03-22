5b0db1a3a93e6ef866690f2060e3eba83c8977bd
Dayana Panova <dzopino@abv.bg>
2021-03-22 22:48:59 +0200

    setting an addIngridient handler and a global state for the price of the ingridients

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,6 +3,13 @@ import Auxilliary from '../../hoc/Auxilliary'
 import Burger from '../../components/Burger/Burger'
 import BuildControls from '../../components/Burger/BuildControls/BuildControl/BuildControls'
 
+const INGRIDIENTS_PRICES = {
+    salad: 0.5,
+    bacon: 0.7,
+    cheese: 0.5,
+    meat: 1.3
+}
+
 class BurgerBuilder extends Component {
     state = {
         ingridients: {
@@ -10,8 +17,23 @@ class BurgerBuilder extends Component {
             bacon: 0,
             cheese: 0,
             meat:0
-        }
+        },
+        totalPrice: 4,
     }
+
+     addIngridientHandler = (type) => {
+        const oldCount = this.state.ingridients[type];
+        const updatedCount = oldCount +1;
+        const updatedIngridients = {
+            ...this.state.ingridients
+        }
+        updatedIngridients[type] = updatedCount;
+    }
+
+    removeIngridientHandler = (type) => {
+
+    }
+
     render() {
         return (
             <Auxilliary>
