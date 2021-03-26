77a7095705283723d031199ad7636011862e03c0
Dayana Panova <dzopino@abv.bg>
2021-03-26 12:02:36 +0200

    adding styles to sidedrawer, styling the navigation items

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -3,13 +3,14 @@ import classes from './Toolbar.css'
 import Logo from '../../Logo/Logo'
 import NavigationItems from '../NavigationItems/NavigationItems'
 
+
 const toolbar = () => (
     <header className={classes.Toolbar}>
         <div>MENU</div>
         <div className={classes.Logo}>
             <Logo height='80%'/>
         </div>
-        <nav>
+        <nav className={classes.DesktopOnly}>
             <NavigationItems/>
         </nav>
     </header>
