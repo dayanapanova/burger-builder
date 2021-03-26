1f97dbb4e5c3b18a3e3ca5668292a3b948ed3de1
Dayana Panova <dzopino@abv.bg>
2021-03-26 10:40:59 +0200

    adding a navigation items and navigation item component with props link and active classes and adding them in the toolbar component

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import classes from './NavigationItem.css'
 
 const navigagationItem = (props) => (
-    <li><a href='/'>Link</a></li>
+    <li className={classes.NavigationItem}>
+        <a href={props.link}
+        className={props.active ? classes.active : null}>
+        {props.children}</a>
+    </li>
 )
+
+export default navigagationItem;
--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import classes from './NavigationItems.css'
+import NavigationItem from './NavigationItem/NavigationItem'
 
 const navigationItems = (props) => (
-    <ul>
-        <li>
-            <a href='/'>Link</a>
-        </li>
+    <ul className={classes.NavigationItems}>
+        <NavigationItem link='/' active>Burger Builder</NavigationItem>
+        <NavigationItem link='/'>Checkout</NavigationItem>
     </ul>
 )
+
+export default navigationItems;
