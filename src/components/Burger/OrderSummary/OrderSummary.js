007d7179f31929ce99472136b44a686988a81a36
Dayana Panova <dzopino@abv.bg>
2021-03-26 15:07:47 +0200

    test with componentDidMount

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,28 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Auxilliary from '../../../hoc/Auxilliary';
 import Button from '../../UI/Button/Button'
 
-const orderSummary = (props) => {
-    const ingridientsSummary = Object.keys(props.ingridients)
-    .map(igKey => {
+class OrderSummary extends Component {
+    componentDidUpdate() {
+        console.log('[Order Summary] WillUpdate');
+    }
+    render() {
+        const ingridientsSummary = Object.keys(this.props.ingridients)
+        .map(igKey => {
         return (
-        <li key={igKey}>
-                <span style={{textTransform: 'capitalise'}}>{igKey}</span>: {props.ingridients[igKey]}
-        </li>
-    )})
-    return (
-        <Auxilliary>
-        <h3>Your order</h3>
-        <p>A delicious burger with the following ingridients</p>
-        <ul>
-            {ingridientsSummary}
-        </ul>
-        <p><strong>Total price: {props.price.toFixed(2)}</strong></p>
-        <p>Countinue to checkout ?</p>
-        <Button btnType='Danger' clicked={props.purchaseCancelled}>Cancel</Button>
-        <Button btnType='Success' clicked={props.purchaseCountinued}>Countinue</Button>
-    </Auxilliary>
-    )
+            <li key={igKey}>
+                    <span style={{textTransform: 'capitalise'}}>{igKey}</span>: {this.props.ingridients[igKey]}
+            </li> )
+        })
+        return(
+            <Auxilliary>
+                <h3>Your order</h3>
+                <p>A delicious burger with the following ingridients</p>
+                <ul>
+                    {ingridientsSummary}
+                </ul>
+                <p><strong>Total price: {this.props.price.toFixed(2)}</strong></p>
+                <p>Countinue to checkout ?</p>
+                <Button btnType='Danger' clicked={this.props.purchaseCancelled}>Cancel</Button>
+                <Button btnType='Success' clicked={this.props.purchaseCountinued}>Countinue</Button>
+        </Auxilliary>
+        )
+    }
 }
-
-export default orderSummary;
+export default OrderSummary;
