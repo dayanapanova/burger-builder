afaf32b1ce4356424fc27dde2ba2ff66a0102eeb
Dayana Panova <dzopino@abv.bg>
2021-03-30 21:42:38 +0300

    installing package react-router-dom and adding browser router to index.js. After then adding switch and route to app.js to navigate trough pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import Layout from './components/Layout/Layout'
-import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
+import { Route, Switch } from 'react-router-dom';
+import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
 
 class App extends Component {
@@ -8,9 +9,11 @@ class App extends Component {
     return (
       <div >
         <Layout>
-          <BurgerBuilder/>
-          <Checkout/>
-        </Layout>
+          <Switch>
+            <Route path='/checkout' component={Checkout}/>
+            <Route path='/' exact component={BurgerBuilder}/>
+          </Switch>
+          </Layout>
       </div>
     );
   }
