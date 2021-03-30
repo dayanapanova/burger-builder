136ab5e90531ef85e75ae01b7e54ce6e4d57541f
Dayana Panova <dzopino@abv.bg>
2021-03-30 21:30:28 +0300

    adding the checkout component to app.js

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -7,7 +7,7 @@ const checkoutSummary = (props) => {
     return(
         <div className={classes.CheckoutSummary}>
             <h1>We hope it taste delicious!</h1>
-            <div style={{width='300px', height='300px', margin='auto'}}>
+            <div style={{width: '100%', height: '300px', margin: 'auto'}}>
                 <Burger ingridients={props.ingridients}/>
             </div>
             <Button btnType='Danger'>CANCEL</Button>
