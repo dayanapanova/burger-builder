f120b030229985f9c7870d46d78d94e3ad96f5de
Dayana Panova <dzopino@abv.bg>
2021-03-30 21:03:33 +0300

    styling the checkout summary component and adding it to checkout component

--- src/components/Checkout/Checkout.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-class Checkout extends Component {
-    render() {
-
-    }
-}
-
-export default Checkout;
--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import Burger from '../../Burger/Burger';
+import Button from '../../UI/Button/Button';
+import classes from './CheckoutSummary.css'
 
 const checkoutSummary = (props) => {
-
+    return(
+        <div className={classes.CheckoutSummary}>
+            <h1>We hope it taste delicious!</h1>
+            <div style={{width='300px', height='300px', margin='auto'}}>
+                <Burger ingridients={props.ingridients}/>
+            </div>
+            <Button btnType='Danger'>CANCEL</Button>
+            <Button btnType='Succsses'>Countinue</Button>
+        </div>
+    )
 }
 
 export default checkoutSummary;
