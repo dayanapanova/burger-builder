42cf1b04771ccfba561f56e45c1d56f086f0b4ca
Dayana Panova <dzopino@abv.bg>
2021-04-01 23:22:18 +0300

    dynamiccaly creates output based on JS config

--- src/containers/Checkout/CheckoutData/CheckoutData.js
+++ src/containers/Checkout/CheckoutData/CheckoutData.js
@@ -6,23 +6,76 @@ import Input from '../../../components/Input/Input';
 
 class CheckoutData extends Component {
     state = {
-        name: '',
-        email: '',
-        adress: {
-            street: '',
-            postalCode: ''
+        orderForm: {
+            name: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Your name'
+                },
+                value: ''
+            },
+            street: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Street'
+                },
+                value: ''
+            },
+            zipCode: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Zip code'
+                },
+                value: ''
+            },
+            country: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Country'
+                },
+                value: ''
+            },
+            email: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'email',
+                    placeholder: 'Your email'
+                },
+                value: ''
+            },
+            deliveryMethod: {
+                elementType: 'select',
+                elementConfig: {
+                    options: [{value: 'fastest', displayValue: 'Fastest'}, {value: 'chiepest', displayValue: 'Chiepest'}],
+                },
+                value: ''
+            }
+
         }
     }
 
     render() {
+        const formElementArray = [];
+        for (let key in this.state.orderForm) {
+            formElementArray.push({
+                id: key,
+                config: this.state.orderForm[key]
+            })
+        }
         return(
             <div className={classes.CheckoutData}>
                 <h4>Enter your contact data</h4>
                 <form>
-                    <Input inputtype='input' type='text' name='name' placeholder='Name'/>
-                    <Input inputtype='input' type='email' name='email' placeholder='Email'/>
-                    <Input inputtype='input' type='text' name='street' placeholder='Street'/>
-                    <Input inputtype='input' type='text' name='postalCode' placeholder='PostalCode'/>
+                    {formElementArray.map(formElement => (
+                        <Input  key={formElement.id}
+                                elementType={formElement.config.elementType}
+                                elementConfig={formElement.config.elementConfig}
+                                value={formElement.config.value}/>
+                    ))}
                     <Button btnType='Success'>ORDER</Button>
                 </form>
             </div>
